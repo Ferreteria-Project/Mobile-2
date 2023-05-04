0d5a6cd7a40f02a23fb7c629821593348393f0b5
PotCAR1452 <oscardanielrml@gmail.com>
2023-05-03 22:15:43 -0600

    panatallainicioycarrusel

--- app/src/main/java/com/example/plantillaregistrarseari/Pantalla_principal_cliente.kt
+++ app/src/main/java/com/example/plantillaregistrarseari/Pantalla_principal_cliente.kt
@@ -2,14 +2,18 @@ package com.example.plantillaregistrarseari
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ViewFlipper
 
 class Pantalla_principal_cliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal_cliente)
 
-
-
+        val viewFlipper: ViewFlipper = findViewById(R.id.viewFlipper)
+        viewFlipper.setInAnimation(this, android.R.anim.fade_in)
+        viewFlipper.setOutAnimation(this, android.R.anim.fade_out)
+        viewFlipper.flipInterval = 3000
+        viewFlipper.isAutoStart = true
 
     }
 }

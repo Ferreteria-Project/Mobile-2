a274ee533082f5c75f9820d53f019fafae1cd73a
PotCAR1452 <oscardanielrml@gmail.com>
2023-05-03 21:37:21 -0600

    pantakka de incio y splash pantalla de agregar productops

---
+++ app/src/main/java/com/example/plantillaregistrarseari/Pantalla_principal_cliente.kt
@@ -0,0 +1,15 @@
+package com.example.plantillaregistrarseari
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_principal_cliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_principal_cliente)
+
+
+
+
+    }
+}
